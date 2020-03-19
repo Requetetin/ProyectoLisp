1c3a08b8395a2e37895917f24ebdea748b41e4aa
Andrea <andreaa.malin@gmail.com>
2020-03-19 08:04:04 -0600

    Arreglos

---
+++ src java/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		InterpreteLisp interprete = new InterpreteLisp();
+		Boolean seguir = true;
+		
+		while(seguir){
+			String text = "";
+			System.out.print("user> ");
+
+			while(scan.hasNextLine()){
+				String input = scan.nextLine();
+				if(input == null || input.isEmpty()){
+					break; 
+    			}
+    			text += input;
+			}
+
+			if (text.equals("")) {
+				seguir = false;
+			} else {
+				System.out.println(interprete.buscarFuncionLisp(text)); //Dependiendo del metodo encontrado 
+			}
+			
+		}
+	}
+}
