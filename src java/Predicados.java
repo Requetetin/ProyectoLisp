275ea72148bfa3cc2ece5ef1d2ffdb2b6bb58210
Requetetin <martinamadog@gmail.com>
2020-03-19 09:59:13 -0600

    Funcion cond en Predicados

--- src java/Predicados.java
+++ src java/Predicados.java
@@ -52,19 +52,175 @@ public class Predicados{
 	}
 
 	public String funCond(ArrayList<ArrayList<String>> listado){
-		/*
+		String s = "";
 		for(int i=0; i<listado.size();i++){
 			if(listado.get(i).contains("cond")){
-				if(listado.get(i-2)){
-					return listado.get(i-1).get(1);
-				}else if(listado.get(i-4)){
-					return listado.get(i-3).get(1);
-				}else{
-					return listado.get(i-5).get(2);
+				Integer a = 0;
+				Integer b = 0;
+				String c = "";
+
+				a = Integer.parseInt(listado.get(i-2).get(2));
+				b = Integer.parseInt(listado.get(i-2).get(3));
+				c = listado.get(i-2).get(1);
+
+				switch(c){
+					case ">":
+					if(a > b){
+						for(int j=1;j<listado.get(i-1).size()-1;j++){
+						s+= listado.get(i-1).get(j);
+						}
+						return s;
+					}else{
+						a = Integer.parseInt(listado.get(i-4).get(2));
+						b = Integer.parseInt(listado.get(i-4).get(3));
+						c = listado.get(i-4).get(1);
+						switch(c){
+							case ">":
+								if(a > b){
+								for(int j=1;j<listado.get(i-3).size()-1;j++){
+									s+= listado.get(i-3).get(j);
+									}	
+								}else{
+								for(int j=1;j<listado.get(i-6).size()-1;j++){
+									s+= listado.get(i-6).get(j);
+									}
+								}
+							 break;
+							case "<":
+								if(a < b){
+								for(int j=1;j<listado.get(i-3).size()-1;j++){
+									s+= listado.get(i-3).get(j);
+									}	
+								}else{
+								for(int j=1;j<listado.get(i-6).size()-1;j++){
+									s+= listado.get(i-6).get(j);
+									}
+								}
+							 break;
+
+							case "=":
+								if(a == b){
+								for(int j=1;j<listado.get(i-3).size()-1;j++){
+									s+= listado.get(i-3).get(j);
+									}	
+								}else{
+								for(int j=1;j<listado.get(i-6).size()-1;j++){
+									s+= listado.get(i-6).get(j);
+									}
+								}
+							 break;
+						}
+					}
+					
+					
+					 break;
+
+					case "<":
+					if(a < b){
+						for(int j=1;j<listado.get(i-1).size()-1;j++){
+						s+= listado.get(i-1).get(j);
+						}
+						return s;
+					}else{
+						a = Integer.parseInt(listado.get(i-4).get(2));
+						b = Integer.parseInt(listado.get(i-4).get(3));
+						c = listado.get(i-4).get(1);
+						switch(c){
+							case ">":
+								if(a > b){
+								for(int j=1;j<listado.get(i-3).size()-1;j++){
+									s+= listado.get(i-3).get(j);
+									}	
+								}else{
+								for(int j=1;j<listado.get(i-6).size()-1;j++){
+									s+= listado.get(i-6).get(j);
+									}
+								}
+							 break;
+							case "<":
+								if(a < b){
+								for(int j=1;j<listado.get(i-3).size()-1;j++){
+									s+= listado.get(i-3).get(j);
+									}	
+								}else{
+								for(int j=1;j<listado.get(i-6).size()-1;j++){
+									s+= listado.get(i-6).get(j);
+									}
+								}
+							 break;
+
+							case "=":
+								if(a == b){
+								for(int j=1;j<listado.get(i-3).size()-1;j++){
+									s+= listado.get(i-3).get(j);
+									}	
+								}else{
+								for(int j=1;j<listado.get(i-6).size()-1;j++){
+									s+= listado.get(i-6).get(j);
+									}
+								}
+							 break;
+						}
+					}
+					
+
+					case "=":
+					if(a == b){
+						for(int j=1;j<listado.get(i-1).size()-1;j++){
+						s+= listado.get(i-1).get(j);
+						}
+						return s;
+					}else{
+						a = Integer.parseInt(listado.get(i-4).get(2));
+						b = Integer.parseInt(listado.get(i-4).get(3));
+						c = listado.get(i-4).get(1);
+						switch(c){
+							case ">":
+								if(a > b){
+								for(int j=1;j<listado.get(i-3).size()-1;j++){
+									s+= listado.get(i-3).get(j);
+									}	
+								}else{
+								for(int j=1;j<listado.get(i-6).size()-1;j++){
+									s+= listado.get(i-6).get(j);
+									}
+								}
+							 break;
+							case "<":
+								if(a < b){
+								for(int j=1;j<listado.get(i-3).size()-1;j++){
+									s+= listado.get(i-3).get(j);
+									}	
+								}else{
+								for(int j=1;j<listado.get(i-6).size()-1;j++){
+									s+= listado.get(i-6).get(j);
+									}
+								}
+							 break;
+
+							case "=":
+								if(a == b){
+								for(int j=1;j<listado.get(i-3).size()-1;j++){
+									s+= listado.get(i-3).get(j);
+									}	
+								}else{
+								for(int j=1;j<listado.get(i-6).size()-1;j++){
+									s+= listado.get(i-6).get(j);
+									}
+								}
+							 break;
+						}
+					}
+					
+
+
 				}
-			}
+
+				
+			}	
+		
+		
 		}
-		*/
-		return "era para ver si funcionaba lo otro";
+		return "Fallo funcion cond";
 	}	
 }
