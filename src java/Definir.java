a195d43bbb52329e150b676db544362a62b9a2fa
Brandon Hernández <brandonhernandez_10@outlook.es>
2020-03-20 14:16:04 -0600

    Ya se comunica para recursividad

--- src java/Definir.java
+++ src java/Definir.java
@@ -13,12 +13,9 @@ public class Definir{
 	// Se guardaran aquí las funciones y las variables
 	private Map<String, ArrayList<String>> functions = new HashMap<>();
 	private Map<String, ArrayList<String>> variables = new HashMap<>();
+	private Map<String, ArrayList<ArrayList<String>>> oldFunction = new HashMap<>();
 	private ArrayDeque<String> container = new ArrayDeque<>();
-
-	// Instancias personales
-	private Calcular calc = new Calcular();
-	private Predicados predicate = new Predicados();
-
+	
 	/**
 	 * Se encarga de correr una función o indicar que no se encuentra
 	 * @pos se ejecuta una función o se manda un error
@@ -27,16 +24,16 @@ public class Definir{
 	 */
 	public String runFuncion(ArrayList<ArrayList<String>> function){
 		// Espacios en donde se guardaran las funciones y las variables
+		Calcular calc = new Calcular();
 		ArrayList<String> funtionality = new ArrayList<>();
-		ArrayList<String> variables = new ArrayList<>();
-		ArrayList<String> replaceVariables = new ArrayList<>();
+		ArrayList<String> variablesM = new ArrayList<>(); // Las que se agarran del mapa
+		ArrayList<String> replaceVariables = new ArrayList<>(); // Las que manda el usuario
 
 		// Para realizar las operaciones matematicas
 		ArrayList<String> operadoresAritmeticos = new ArrayList<>();
 
-
 		// Banderas
-		Boolean logic = false, aritmetico = false; // Verifica si hay un cond()
+		Boolean logic = false, aritmetico = false, recursive = false; // Verifica si hay un cond()
 		String resultado = "HOLA BB <3 :3";
 
 		// Agregando los operadores matematicos
@@ -50,15 +47,18 @@ public class Definir{
 		operadoresAritmeticos.add("/");
 		//-------------------------------------	
 
-
+		//---------------------------------------------------------
 		String functionName = function.get(0).get(1).toUpperCase(); // Nombre en mayusculas de la funcion
 		function.get(0).remove(1);
 		function.get(0).remove("(");
 		function.get(0).remove(function.size());
+		//---------------------------------------------------------
 
 		// Consiguiendo los elementos necesarios para poder realizar la funcion
-		funtionality = functions.get(functionName);
-		variables = this.variables.get(functionName);
+		for(int i = 0; i < functions.get(functionName).size(); i++){
+			funtionality.add(functions.get(functionName).get(i)); // Copiando lo que hay en el mapa
+		}
+		variablesM = variables.get(functionName);
 
 		// Consiguiendo las variables del arraylist de arraylist
 		for(int i = 0; i < function.get(0).size(); i++){
@@ -66,32 +66,28 @@ public class Definir{
 		}
 
 		// Verificando que tenga la misma longitud de parametros que variables en la funcion
-		if(replaceVariables.size() == variables.size()){
+		if(replaceVariables.size() == variablesM.size()){
 			
 			// Cambiando todas las variables por números mandados por el usuario
 			for(int i = 0; i < funtionality.size(); i++){ // Recorriendo el arraylist
 				
 				for(int j = 0; j < replaceVariables.size(); j++){ // Trata de remplazar si es una variable
-					if(funtionality.get(i).equalsIgnoreCase(variables.get(j))){
+					if(funtionality.get(i).equalsIgnoreCase(variablesM.get(j))){
 						funtionality.set(i, replaceVariables.get(j));
 					}
 
-					if(funtionality.get(i).equalsIgnoreCase(" cond")){ // Verificando si tiene un cond
+					if(funtionality.get(i).equalsIgnoreCase("cond")){ // Verificando si tiene un cond
 						logic = true;
 					}
 
 				}
-				
 			}
-
-			System.out.println("---- K " + funtionality);
 
 			// Empieza la ejecución del programa verificando en donde empieza
 			if(logic){
+				funtionality = predicateCommunication(functionName, replaceVariables);
 
-
-
-
+				return "AUN NO PAPA";
 			}else{
 
 				// Realizando sin recursividad 
@@ -110,18 +106,67 @@ public class Definir{
 
 				}
 
+				// Consiguiendo el valor final 
+				resultado = container.removeFirst();
+
+				return resultado;
 			}
 
-
-			// Consiguiendo el valor final 
-			resultado = container.removeFirst();
-
-			return resultado;
 		}else{ // Cuando ! [x,y].size() == [3, 3, 8].size() 
 			return "*** - EVAL/APPLAY: se han entregado demasiados argumentos a " + functionName;
 
 		}
 		
+	}
+
+
+	/**
+	* Se encarga de comunicarse con la clase de predicado 
+	* @pre se tiene una operacion logica
+	* @pos se tiene una operacion aritmetica
+	* @param functionName es el nombre de la funcion que se esta corriendo 
+	* @param replaceVariables son las variables que se deben de cambiar al momento de correr la funcion
+	* @return devuelve un arraylist que puede ser ejecutado por el programa
+	*/
+	private ArrayList<String> predicateCommunication(String functionName, ArrayList<String> replaceVariables){
+		Predicados predicate = new Predicados();
+		ArrayList<ArrayList<String>> communicate = new ArrayList<>();
+		ArrayList<String> aux = new ArrayList<>();
+		String[] temp;
+		String response;
+
+		for(int i = 0; i < this.oldFunction.get(functionName).size(); i++){
+			communicate.add(this.oldFunction.get(functionName).get(i));
+		}
+				
+		// Remplazando todas las variables por números
+		for(int i = 0; i < communicate.size(); i++){ // Por cada elemento del arraylist
+			for(int j = 0; j < communicate.get(i).size(); j++){ // Por cada string en el arraylist
+
+				for(int k = 0; k < replaceVariables.size(); k++){ // Por cada variable
+
+					if(communicate.get(i).get(j).equalsIgnoreCase(variables.get(functionName).get(k))){
+						communicate.get(i).set(j, replaceVariables.get(k)); // Remplazando las variables por números
+					}
+
+				}
+
+			}
+		}
+
+		System.out.println("Predicado\n" + communicate);
+		// Consiguiendo el string que tendra que convertirse en arraylist para ser operado
+		response = predicate.funCond(this.oldFunction.get(functionName));
+
+		temp = response.split(" ");
+
+		for(int i = 0; i < temp.length; i++){
+			aux.add(temp[i]);
+		}
+
+		System.out.println(aux);
+
+		return aux;
 	}
 
 	/**
@@ -140,7 +185,6 @@ public class Definir{
 			aux.add(container.removeFirst());			
 		}
 
-		System.out.println(aux);
 		dReturn.add(aux);
 		return dReturn;
 	} 
@@ -154,7 +198,7 @@ public class Definir{
 		String temp, aux;
 
 		try{
-			Integer.parseInt(container.element()); // Verificando si es numero u operador
+			Float.parseFloat(container.element()); // Verificando si es numero u operador
 
 			// Cambiando los valores para que quede el operador aritmetico en la primera posicion
 			temp = container.removeFirst(); // Numero
@@ -179,6 +223,8 @@ public class Definir{
 	}
 
 
+
+
 	/**
 	* Se encarga de agregar una nueva funcin al mapa
 	* @pre hay una n cantidad de funciones y variables
@@ -189,29 +235,34 @@ public class Definir{
 	public String setFuncion(ArrayList<ArrayList<String>> nuevaFuncion){
 		ArrayList<String> variablesF = new ArrayList<>();
 		ArrayList<String> funtion = new ArrayList<>();
-		String funtionName;
+		String functionName;
 
 		// Consiguiendo el nombre y las funciones
 		variablesF = setNombreFuncion(nuevaFuncion.get(nuevaFuncion.size() - 1));
-		funtionName = variablesF.get(0);
+		functionName = variablesF.get(0);
 		
 		// Eliminando el nombre y cambiandolo a mayuscular
 		variablesF.remove(0);
-		funtionName = funtionName.toUpperCase();
+		functionName = functionName.toUpperCase();
 
 		funtion = setFuncionalidadFuncion(nuevaFuncion); // Consiguiendo el funcionamiento de la función
+		
+		nuevaFuncion.remove(nuevaFuncion.size() - 1); // Eliminando la ultima posicion
 
 		// Agregando las funciones 
-		this.functions.put(funtionName, funtion);
-		this.variables.put(funtionName, variablesF);
+		this.functions.put(functionName, funtion);
+		this.variables.put(functionName, variablesF);
+		this.oldFunction.put(functionName, nuevaFuncion);
 
 		/**
 		Borrar despues
 		*/
 		System.out.println("\nInfo Metodo:");
-		System.out.println(this.functions.get(funtionName));
-		System.out.println(this.variables.get(funtionName));
-		return funtionName;
+		System.out.println(this.functions.get(functionName));
+		System.out.println(this.variables.get(functionName));
+		System.out.println(this.oldFunction.get(functionName));
+
+		return functionName;
 	}
 
 	/**
