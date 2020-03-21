9e15f8acbff9cc0e09d95c35392b973a761c7c2d
Brandon Hernández <brandonhernandez_10@outlook.es>
2020-03-20 23:52:40 -0600

    JAVADOC

---
+++ src java/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Calcular"},{"p":"<Unnamed>","l":"Definir"},{"p":"<Unnamed>","l":"InterpreteLisp"},{"p":"<Unnamed>","l":"Leer"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Predicados"}]
